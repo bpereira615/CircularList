52b3c3b4e25124ae04f99cfce6a5781e92e206fa
bpereira615 <bpereira615@gmail.com>
2016-02-11 21:41:31 -0500

    Cursor manipulation functions complete, to be tested

--- src/CList.java
+++ src/CList.java
@@ -1,5 +1,6 @@
 /********************************************************************
  * Benjamin Hoertnagl-Pereira bhoertn1 631-488-7197 bhoertn1@jhu.edu
+ * Lydia Carroll lcarro12 240-994-8355 lcarro12@jhu.edu
  *
  * 600.226.02 CS226 Data Structures
  * Project 1 - CircularList
@@ -13,7 +14,7 @@
  *
  * @author Joanne Selinski
  * @author Benjamin Hoertnagl-Pereira
- * 
+ * @author Lydia Carrol
  * @param <T>
  *            the type of the List
  */
@@ -68,8 +69,8 @@ public class CList<T> implements List<T> {
      */
     public void clear() {
         this.size = 0;
-        this.head = null; //new Node(null, null, null);
-        this.curr = null; //this.head; // because insert will insert after curr
+        this.head = null; // new Node(null, null, null);
+        this.curr = null; // this.head; // because insert will insert after curr
     }
 
     /**
@@ -81,24 +82,24 @@ public class CList<T> implements List<T> {
      * @return true if successfully inserted, false otherwise
      */
     public boolean insert(T t) {
-    	if (this.size == 0) {
-    		// need to make an initial node
-    		this.head = new Node(t, null, null);
-    		// set prev to current node, aka head
-    		this.prev = this.head;
-    		// also set next to current b/c only one node
-    		this.next = this.head;
-    		this.size++;
-    		this.curr = this.head;
-    		return true;
-    	} else {
-    		Node n = new Node(t, this.curr, this.curr.next);
-    		n.prev.next = n; // connect left neighbor
-    		n.next.prev = n; // connect right neighbor
-    		this.size++;
-    		this.curr = n; // move cursor
-    		return true;
-    	}
+        if (this.size == 0) {
+            // need to make an initial node
+            this.head = new Node(t, null, null);
+            // set prev to current node, aka head
+            this.head.prev = this.head;
+            // also set next to current b/c only one node
+            this.head.next = this.head;
+            this.size++;
+            this.curr = this.head;
+            return true;
+        } else {
+            Node n = new Node(t, this.curr, this.curr.next);
+            n.prev.next = n; // connect left neighbor
+            n.next.prev = n; // connect right neighbor
+            this.size++;
+            this.curr = n; // move cursor
+            return true;
+        }
     }
 
     /**
@@ -110,54 +111,57 @@ public class CList<T> implements List<T> {
      * @return true if successfully appended, false otherwise
      */
     public boolean append(T t) {
-    	if (this.size == 0) {
-    		this.insert(t); // already built zero case into insert
-    		this.size++;
-    		return true;
-    	} else {
-    		Node temp = this.curr; // hold onto original position
-    		this.curr = this.head.prev; // move to the last node (preceding head)
-    		this.insert(t); // code reuse!
-    		this.curr = temp; // restore cursor to original position
-    		this.size++;
-    		return true;
-    	}
+        if (this.size == 0) {
+            this.insert(t); // already built zero case into insert
+            this.size++;
+            return true;
+        } else {
+            Node temp = this.curr; // hold onto original position
+            this.curr = this.head.prev; // move to the last node (preceding
+                                        // head)
+            this.insert(t); // code reuse!
+            this.curr = temp; // restore cursor to original position
+            this.size++;
+            return true;
+        }
     }
 
     /**
-     * Remove current item and return it
-     * Set cursor to next item (one to right of removed).
+     * Remove current item and return it Set cursor to next item (one to right
+     * of removed).
      * 
      * @return the value of the element removed, null if list is empty
      */
     public T remove() {
-    	// do we have a tail?
-    	// f this
+        // do we have a tail?
+        // f this
         // if (this.curr.next == this.tail) {
-        //    return null;
-        //}
-    	if (this.size == 0){
-    		return null;
-    	} else {
-	        T val = this.curr.data;
-	        this.curr.next.prev = this.curr.prev; // bypass node being deleted
-	        this.curr.prev.next = this.curr.next; // bypass it in other direction
-	        this.size--;
-	        this.curr = this.curr.next; // set cursor to next node
-	        return val;
-    	}
+        // return null;
+        // }
+        if (this.size == 0) {
+            return null;
+        } else {
+            T val = this.curr.data;
+            this.curr.next.prev = this.curr.prev; // bypass node being deleted
+            this.curr.prev.next = this.curr.next; // bypass it in other
+                                                  // direction
+            this.size--;
+            this.curr = this.curr.next; // set cursor to next node
+            return val;
+        }
     }
 
     /**
-     * Return the current element (data to right of cursor).
+     * Return the current element.
      * 
      * @return the value of the current element, null if none
      */
     public T getValue() {
-        if (this.curr.next == this.tail) {
+        if (this.size == 0) {
+            // TODO: error statement to be added
             return null;
         }
-        return this.curr.next.data;
+        return this.curr.data;
     }
 
     /**
@@ -169,18 +173,18 @@ public class CList<T> implements List<T> {
         return this.size;
     }
 
-    /* ---------- METHODS BELOW THIS LINE ARE NOT IMPLEMENTED ------------ */
-
     /**
      * Set the current position to the start of the list.
      */
     public void moveToStart() {
+        this.curr = this.head;
     }
 
     /**
      * Set the current position to the end of the list.
      */
     public void moveToEnd() {
+        this.curr = this.head.prev;
     }
 
     /**
@@ -188,12 +192,24 @@ public class CList<T> implements List<T> {
      * beginning.
      */
     public void prev() {
+        // TODO: check if this is faulty in comparison operation
+        if (this.curr != this.head) {
+            this.curr = this.curr.prev;
+        } else {
+            System.out.println("ERROR: At head of list!");
+        }
     }
 
     /**
      * Move the current position one step right, no change if already at end.
      */
     public void next() {
+        // TODO: what is defined as end
+        if (this.curr != this.head.prev) {
+            this.curr = this.curr.next;
+        } else {
+            System.out.println("ERROR: At end of list!");
+        }
     }
 
     /**
@@ -202,20 +218,40 @@ public class CList<T> implements List<T> {
      * @return the current position in the list
      */
     public int currPos() {
-        // dummy implementation
-        return 0;
+        // at the head
+        int position = 0;
+
+        // temporary variable for current position
+        Node temp = this.curr;
+        this.moveToStart(); // code reuse!
+
+        // iterate through list until current node matches original current node
+        while (this.curr != temp) {
+            position++;
+            this.curr = this.curr.next;
+        }
+
+        return position;
     }
 
     /**
-     * Set the current position.
+     * Set the current position, indexing from 0.
      * 
      * @param pos
      *            the value to set the position to
      * @return true if successfully changed position, false otherwise
      */
     public boolean moveToPos(int pos) {
-        // dummy implementation
-        return false;
+        // TODO: return false case?
+
+        this.moveToStart(); // code reuse!
+
+        for (int i = 0; i < pos; i++) {
+            // move the cursor to next position
+            this.next(); // code reuse
+        }
+
+        return true;
     }
 
     /**
@@ -224,8 +260,7 @@ public class CList<T> implements List<T> {
      * @return true if the current position is the end of the list
      */
     public boolean isAtEnd() {
-        // dummy implementation
-        return false;
+        return (this.curr == this.head.prev);
     }
-
+    /* ---------- METHODS BELOW THIS LINE ARE NOT IMPLEMENTED ------------ */
 }
