dbfc76e66ccb7936de1e8d7fcf1af6f82a178d9b
bpereira615 <bpereira615@gmail.com>
2016-02-12 23:39:59 -0500

    Item and Station temporary dummy variables

--- src/CookingItem.java
+++ src/CookingItem.java
@@ -9,4 +9,31 @@
 
 public class CookingItem implements CookingItemInterface {
 
+    /**
+     * Implements a simulation of one minute of time for this item by
+     * decrementing cooking time by one minute.
+     */
+    public void tick() {
+
+    }
+
+    /**
+     * Get the time remaining for cooking this dish.
+     * 
+     * @return the time in minutes
+     */
+    public int timeRemaining() {
+        // TODO: dummy return
+        return 0;
+    }
+
+    /**
+     * Calculate the penalty if this dish were removed now.
+     * 
+     * @return the penalty
+     */
+    public int penalty() {
+        // TODO: dummy return
+        return 0;
+    }
 }
--- src/CookingStation.java
+++ src/CookingStation.java
@@ -9,4 +9,36 @@
 
 public class CookingStation implements CookingStationInterface {
 
+    /**
+     * Put a new dish at the end of the staStion.
+     * 
+     * @param it
+     *            the dish to add
+     */
+    public void addItem(CookingItem it) {
+
+    }
+
+    /**
+     * Simulate one minute time passing for this station.
+     */
+    public void tick() {
+
+    }
+
+    /**
+     * Tend the current item
+     * 
+     * @param removeThreshold
+     *            the number of minutes that may be used to determine if an item
+     *            should be removed from the station.
+     * @param penaltyThreshold
+     *            the limit on the penalty value that may be used to determine
+     *            if an item should be removed from the station.
+     * @return the item if you decide to remove it, or null otherwise
+     */
+    public CookingItem tend(int removeThreshold, int penaltyThreshold) {
+        return null;
+    }
+
 }
